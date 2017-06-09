ca2ae32920f6456ea73aeba04a57408c3bd43986
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-06-09 20:35:36 +0100

    Layout added

---
+++ controller.rb
@@ -0,0 +1,28 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+
+require_relative('./models/transaction')
+require_relative('./models/merchant')
+require_relative('./models/tag')
+
+get '/merchant' do
+  @merchants = Merchant.all()
+  erb(:merchant_index)
+end
+
+get '/merchant/new' do
+  #@houses = House.all()
+  erb(:merchant_new)
+end
+
+get '/merchant/:id' do
+  @merchant = Merchant.find(params[:id])
+  erb(:merchant_show)
+end
+
+post '/merchant' do
+  @merchant = Merchant.new(params)
+  @merchant.save()
+  erb(:create)
+end
